a9b9fa0d6892278808d839c63b28820d3ccd26ba
Calico Randall <calicorrandall@gmail.com>
2025-01-15 11:52:08 -0800

    Adding another button

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,18 @@
 #include <Bounce2.h>
 
 #define LED_RING_PIN 15
+#define LED_RING2_PIN 16
 #define TOTAL_LEDS 12
 #define BTN_PIN 3
 
 Bounce2::Button stateButton = Bounce2::Button();
 CRGBArray <TOTAL_LEDS> ledRing;
+CRGBArray <TOTAL_LEDS> ledRing2;
 int state = 0;
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, LED_RING_PIN>(ledRing, TOTAL_LEDS);
+  FastLED.addLeds<NEOPIXEL, LED_RING2_PIN>(ledRing2, TOTAL_LEDS);
 
   stateButton.attach(BTN_PIN, INPUT_PULLUP);
   stateButton.interval(5);
@@ -25,8 +28,8 @@ void rainbow_wave(uint8_t thisSpeed, uint8_t deltaHue) {     // The fill_rainbow
 // uint8_t thisHue = beatsin8(thisSpeed,0,255);                // A simple rainbow wave.
  uint8_t thisHue = beat8(thisSpeed,255);                     // A simple rainbow march.
   
- fill_rainbow(ledRing, TOTAL_LEDS, thisHue, deltaHue);            // Use FastLED's fill_rainbow routine.
- 
+ fill_rainbow(ledRing, TOTAL_LEDS, thisHue, deltaHue);          // Use FastLED's fill_rainbow routine.
+ fill_rainbow(ledRing2, TOTAL_LEDS, thisHue, deltaHue);    
 }
 
 // A simple cycle through white LED and colors
@@ -46,21 +49,25 @@ void loop() {
   switch(state){
     case 0:
       fill_solid(ledRing, TOTAL_LEDS, CRGB::White);
+      fill_solid(ledRing2, TOTAL_LEDS, CRGB::White);
       FastLED.show();
       break;
     
     case 1:
       fill_solid(ledRing, TOTAL_LEDS, CRGB::Red);
+      fill_solid(ledRing2, TOTAL_LEDS, CRGB::Red);
       FastLED.show();
       break;
 
     case 2:
       fill_solid(ledRing, TOTAL_LEDS, CRGB::Green);
+      fill_solid(ledRing2, TOTAL_LEDS, CRGB::Green);
       FastLED.show();
       break;
 
     case 3:
       fill_solid(ledRing, TOTAL_LEDS, CRGB::Blue);
+      fill_solid(ledRing2, TOTAL_LEDS, CRGB::Blue);
       FastLED.show();
       break;
 
